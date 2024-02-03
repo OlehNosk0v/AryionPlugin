bd849278a02338c4edad7d153306f2de3097f46e
OlehNosk0v <158768944+OlehNosk0v@users.noreply.github.com>
2024-02-03 21:18:24 +0300

    Add files via upload

---
+++ AryionPlugins.user.js
@@ -0,0 +1,171 @@
+// ==UserScript==
+// @name         AryionPlugins
+// @namespace    http://tampermonkey.net/
+// @version      1.0.6
+// @description  A simple script for easy viewing of artwork and comics.
+// @license      MIT
+// @author       OlehNoskov
+// @homepage     https://github.com/OlehNoskovCPU/AryionPlugins
+// @updateURL    https://github.com/OlehNoskovCPU/AryionPlugins/raw/main/AryionPlugins.user.js
+// @downloadURL  https://github.com/OlehNoskovCPU/AryionPlugins/raw/main/AryionPlugins.user.js
+// @match        https://aryion.com/g4/view/*
+// @include      https://aryion.com/g4/view/*
+// ==/UserScript==
+
+// Many thanks to Vore Witch for helping with the script!
+
+var index, len;
+
+const DownloadComicPage = async () => {
+    const downloadLink = getDownloadLink()
+    const image = await (await fetch(downloadLink)).blob();
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(image);
+    const title = (document.getElementsByClassName('user-link')[1]).textContent+" - "+(document.getElementsByClassName('g-box-title')[1]).textContent.replace("/", "-");
+    link.download = title;
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+}
+
+const DownloadComics = async () =>{
+    let i = Number(document.getElementById("PagesComics").value);
+    if (i > 0){
+        localStorage.setItem('I', i);
+        await DownloadComicPage();
+        getNextButton().click()
+    }
+}
+
+const getDownloadLink = () => {
+     const button = document.querySelectorAll("div > a");
+     len = button.length
+     for (index = 0; index < len; ++index){
+         if(button[index].innerText == "Download"){
+             return button[index].href;
+         }
+     }
+}
+
+const getNextButton = () => {
+     let button = document.getElementById("next-link")
+     if (!button){
+         //let buttons = document.querySelectorAll("div > a");
+         let buttons = document.querySelectorAll("a.g-button.g-corner-all");
+          for (let i = 0; i < buttons.length - 1; i++){
+              if(buttons[i].innerHTML == "Next &gt;" || buttons[i].innerText == "Next >"){
+               button = buttons[i];
+               break;
+              }
+          }
+      }
+    if (!button){
+        let buttons = document.querySelectorAll("div > a");
+        for (let i = 0; i < buttons.length - 1; i++){
+              if(buttons[i].innerHTML == "Next &gt;" || buttons[i].innerText == "Next >"){
+               button = buttons[i];
+               break;
+              }
+          }
+    }
+    return button
+}
+
+
+
+document.addEventListener('keydown', function(event) {
+  let button = null;
+  if( event.target.nodeName == "INPUT" || event.target.nodeName == "TEXTAREA" ) return;
+  if (event.code == 'ArrowRight') {
+      button = getNextButton()
+  }
+  else if (event.code == 'ArrowLeft'){
+     button = document.getElementById("prev-link")
+      if (button == null){
+          button = document.querySelectorAll("div > a");
+          len = button.length
+          for (index = 0; index < len; ++index){
+              if(button[index].innerHTML == "&lt; Prev"){
+               button = button[index];
+                  break;
+              }
+          }
+      }
+  }
+  else if (event.shiftKey && event.keyCode == 68){
+      void DownloadComicPage();
+  }
+  else if (event.shiftKey && event.keyCode == 70) {
+     button = document.getElementById("fav-link")
+  }
+  if(button){
+     button.click();
+  }
+});
+
+
+const input = document.createElement("input");
+input.type = "number";
+input.value = localStorage.getItem("I");
+input.id = "PagesComics";
+let fragment = document.getElementsByClassName('func-box')[0];
+fragment.appendChild(input);
+const buttoninput = document.createElement("input");
+buttoninput.type = "button";
+buttoninput.id = "BUTTONDOWN"
+buttoninput.value = "Download";
+
+fragment = document.getElementsByClassName('func-box')[0];
+fragment.appendChild(buttoninput);
+
+document.getElementById('BUTTONDOWN').onclick = async () => {
+   await DownloadComics();
+};
+
+const blockPage = () => {
+    const waitBanner = document.createElement('div');
+    waitBanner.setAttribute('style', 'z-index: 999999999; position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: #00000055; color: #fff; display: flex; align-items: center; justify-content: center; font-size: 300%; text-shadow: 0 0 25px #000')
+    waitBanner.innerText = 'Wait please, comic is downloading...'
+    waitBanner.id = "BlockPage";
+    document.querySelector('html').setAttribute('style', 'height: 100%; overflow: hidden')
+    document.body.appendChild(waitBanner);
+}
+
+async function delay(ms) {
+  return await new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.addEventListener('load', async () => {
+    let nextButton = await getNextButton();
+    let tryi = 0;
+    for (let tryi = 0; tryi < 30; ++tryi){
+        await delay(100)
+        nextButton = await getNextButton();
+        if (nextButton){
+            break;
+        }
+    }
+    const pagesToDownload = localStorage.getItem('I');
+    console.log("start")
+    if (pagesToDownload >= 1) {
+        console.log(nextButton)
+        await delay(1000)
+        if (!nextButton || nextButton === null) {
+           console.log(nextButton);
+           localStorage.setItem("I", 0);
+        } else {
+            localStorage.setItem('I', pagesToDownload - 1);
+        }
+        if (nextButton) {
+            blockPage();
+        }
+        console.log('working...')
+        await DownloadComicPage();
+        if (nextButton){
+            nextButton.click()
+        }
+    }
+})
---
+++ README.md
@@ -0,0 +1,40 @@
+# AryionPlugins
+## EN:
+This script is written specifically for aryion.com users to simplify life there.
+
+Tampermonkey required: https://www.tampermonkey.net
+
+When Tampermonkey is already installed on your browser, then  [just click this link and the script will install](https://github.com/OlehNoskovCPU/AryionPlugins/raw/main/AryionPlugins.user.js). Tampermonkey will automatically prompt you to install the script.
+
+Hotkeys:
++ LeftArrow/RighArrow - Previous/Next picture.
++ Shift+D - Download image.
++ Shift+F - Add to favourites.
+  
+> [!WARNING]
+> - When downloading via Shift+D, the picture will load in the background, so it may take a long time to load. Depending on your internet speed and the weight of the file, wait from 15 seconds to a minute.
+> - The download function is designed ONLY for Video files, pictures and PDF files.
+
+Also, a field for entering numbers and a Download button will appear next to the buttons. There you enter how many images you want to download, and then click the button.
+
+![AltText](./DownloadPages.png)
+
+## RU:
+Этот скрипт написан специально для пользователей сайта aryion.com, чтобы упростить им жизнь.
+
+Требуется Tampermonkey: https://www.tampermonkey.net
+
+Когда Tampermonkey уже установлен на вашем браузере, то [просто нажми на эту ссылку и скрипт установиться](https://github.com/OlehNoskovCPU/AryionPlugins/raw/main/AryionPlugins.user.js). Tampermonkey автоматически предложит вам установить этот скрипт. 
+
+Горячие клавиши:
++ LeftArrow/RighArrow - Предыдущее/Следующее изображение.
++ Shift+D - Загрузить изображение.
++ Shift+F - Добавить в избранное.
+
+> [!WARNING]
+> - При скачивание через Shift+D картинка будет грузится в фоновом режиме, поэтому она возможно будет долго грузиться. В зависимости от вашего скорости интернета и веса файла подождите от 15 секунд до минуты.
+> - Функция скачивания рассчитано ТОЛЬКО на Видео файлы, картинки и PDF файлы. 
+
+Также рядом с кнопками появиться поле для ввода чисел и кнопка Download. Туда вводится, сколько изображений вы хотите скачать, а после нажимаете кнопку.
+
+![АльтТекст](./DownloadPages.png)
